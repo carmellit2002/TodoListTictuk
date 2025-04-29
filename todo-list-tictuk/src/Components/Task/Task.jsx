4e3100d911b7a7b9cede07e8edb98b53c6a5a09b
Carmel Litmanovich <carmellit2002@gmail.com>
2025-04-29 17:55:51 +0300

    Added styled components

--- todo-list-tictuk/src/Components/Task/Task.jsx
+++ todo-list-tictuk/src/Components/Task/Task.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { TaskContainer, TaskDetailsContainer, Title, Category, CompletionStatus, TaskDetailsContainer, EditTaskButton, DeleteTaskButton } from "./styles";
 
 const Task = ({ title, category, completionStatus }) => {
-    return <div>
-        <div>
-            {title}
-            {category}
-            {completionStatus}
-        </div>
-        {/* edit button */}
-        {/* delete button */}
-    </div>
+    return <TaskContainer>
+        <TaskDetailsContainer>
+            <Title> {title} </Title>
+            <Category> {category} </Category>
+            <CompletionStatus> {completionStatus} </CompletionStatus>
+        </TaskDetailsContainer>
+        <EditTaskButton />
+        <DeleteTaskButton />
+    </TaskContainer>
 }
 
 export default Task;
---
+++ todo-list-tictuk/src/Components/Task/styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const TaskContainer = styled.div`
+
+`;
+
+export const TaskDetailsContainer = styled.div`
+
+`;
+
+export const Title = styled.div`
+
+`;
+
+export const Category = styled.div`
+
+`;
+
+export const CompletionStatus = styled.div`
+
+`;
+
+export const TaskActionsContainer = styled.div`
+
+`;
+
+export const EditTaskButton = styled.button`
+
+`;
+
+export const DeleteTaskButton = styled.button`
+
+`;
