63317d8944bb3fd7d4dbc657ec3c9a7412e30ccb
Carmel Litmanovich <carmellit2002@gmail.com>
2025-04-29 19:52:57 +0300

    Created a header for the application

--- todo-list-tictuk/src/Containers/Homepage/Homepage.jsx
+++ todo-list-tictuk/src/Containers/Homepage/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { HomepageContainer } from "./styles";
+import Header from "../../Components/Header";
 import TasksList from "../TasksList";
 
 const Homepage = () => {
     return <HomepageContainer>
+        <Header />
         <TasksList />
     </HomepageContainer>
 }
