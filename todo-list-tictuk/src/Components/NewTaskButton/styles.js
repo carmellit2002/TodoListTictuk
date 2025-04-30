de9315500720d9d8b3136b099d691b6406144029
Carmel Litmanovich <carmellit2002@gmail.com>
2025-04-30 16:44:04 +0300

    Created a New Task button

---
+++ todo-list-tictuk/src/Components/NewTaskButton/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const NewTaskClickable = styled.button`
+    height: 50%;
+    width: 90%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 8px 12px 8px 12px;
+    gap: 8px;
+
+    border: none;
+    border-radius: 8px;
+    background-color: #5F33E1;
+
+    cursor: pointer;
+`;
+
+export const ButtonText = styled.div`
+    margin-top: 1px;
+
+    font-size: 16px;
+    color: #FFFFFF;
+`;
