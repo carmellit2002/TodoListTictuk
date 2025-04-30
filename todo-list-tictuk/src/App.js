e52dc404e61fa29efb32058017427c2ae2c9e78f
Carmel Litmanovich <carmellit2002@gmail.com>
2025-04-30 16:43:44 +0300

    Installed and added routing between pages

--- todo-list-tictuk/src/App.js
+++ todo-list-tictuk/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Containers/Homepage";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" index element={<Homepage />} />
+          {/* <Route path="/createTask" element={<CreateTaskPage />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
