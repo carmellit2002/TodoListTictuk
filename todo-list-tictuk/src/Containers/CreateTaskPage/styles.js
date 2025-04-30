b22f6e3f28a246ef8c17c28f986405342b45088a
Carmel Litmanovich <carmellit2002@gmail.com>
2025-04-30 19:21:35 +0300

    Created a Create Task Form, conditional rendering for new task button

---
+++ todo-list-tictuk/src/Containers/CreateTaskPage/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CreateTaskPageContainer = styled.div`
+    height: 100vh;
+    width: 100vw;
+    max-width: 100%;
+
+    font-family: 'Inter';
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
